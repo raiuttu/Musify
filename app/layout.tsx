1d202d0340829ade5e3605fd48e4905c982b17b1
raiuttu <05raiuttu@gmail.com>
2024-01-23 16:19:31 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import { Figtree } from 'next/font/google'
+
+import getSongsByUserId from '@/actions/getSongsByUserId'
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
+import Sidebar from '@/components/Sidebar'
+import ToasterProvider from '@/providers/ToasterProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import Player from '@/components/Player'
+
+import './globals.css'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Musify',
+  description: 'Musify',
+}
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const products = await getActiveProductsWithPrices();
+  const userSongs = await getSongsByUserId();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>
+              {children}
+            </Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+        
+      <script src = " https://cdn.botpress.cloud/webchat/v1/inject.js " async>
+      </script>
+      
+      <script src="https://mediafiles.botpress.cloud/7a996cc1-79ae-4abe-9c36-9906bb62f28f/webchat/config.js" defer>
+      </script>
+      </body>
+    </html>
+  )
+}
